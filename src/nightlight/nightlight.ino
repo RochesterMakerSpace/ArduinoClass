a50023df2af2db477f5292e7e010904774e92fe6
Dan McCue <dmccue128@gmail.com>
2019-06-02 06:09:32 -0400

    Add files for the night-light example

---
+++ src/nightlight/nightlight.ino
@@ -0,0 +1,25 @@
+/** 
+ * Arduino Tutorial: Learn how to use analog input and digital output
+ * More info: https://github.com/RochesterMakerSpace/ArduinoClass
+ */
+
+// Initialize the Arduino
+setup() {
+  pinMode(12,OUTPUT);
+}
+
+// Loop forever checking the status of the photoresistor and controlling the LED
+loop() {
+	// Read the photoresistor value (0-1024)
+    int sensorValue = analogRead(A0);
+
+    // Adjust the sensitivity as desired.  Initial value is 500.
+    if (sensorValue < 500) {
+		// It's dark.  Turn on the LED.
+        digitalWrite(12,HIGH);
+    } else {
+		// It's not too dark.  Turn off the LED.
+        digitalWrite(12,LOW);
+    }
+
+}
